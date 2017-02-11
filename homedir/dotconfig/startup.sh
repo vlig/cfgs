97de4771aaf6da454b0c00770675a932f2bd1070
Vladimir Smirnov <vladimir.igo@gmail.com>
2017-02-11 23:13:13 +0300

    Update startup.sh

--- homedir/dotconfig/startup.sh
+++ homedir/dotconfig/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# ~/.config/autostart-scripts/startup.sh
 
 mplayer -really-quiet -vc null -vo null ~/Music/logonsound/* </dev/null &
 mount ~/mnt/yadisk 
 sleep 20 && kstart --skiptaskbar kontact --iconify
-sleep 10 && kstart --skiptaskbar nixnote2
+#sleep 10 && kstart --skiptaskbar nixnote2
+sc-deskstop.py start
