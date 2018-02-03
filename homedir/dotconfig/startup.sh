fe01207ebe1e13dc45990470a9d4686b2b372dfe
Vladimir Smirnov <vladimir.igo@gmail.com>
2018-02-03 22:47:34 +0300

    Update startup.sh

--- homedir/dotconfig/startup.sh
+++ homedir/dotconfig/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-mplayer -really-quiet -vc null -vo null ~/Music/logonsound/* </dev/null &
+#xinput --set-prop 9 'libinput Accel Profile Enabled' 1, 0
+#mplayer -really-quiet -vc null -vo null ~/Music/logonsound/* </dev/null &
+plaympeg ~/Music/logonsound/* </dev/null &
 mount ~/mnt/yadisk 
-sleep 20 && kstart --skiptaskbar kontact --iconify
+#sleep 20 && kstart --skiptaskbar kontact --iconify
 #sleep 10 && kstart --skiptaskbar nixnote2
 sc-deskstop.py start
+ktorrent && wmctrl -c "ktorrent"
