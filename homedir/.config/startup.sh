83be88e45470d1a7d327403284bb7b3d4d3fee28
vlig <vladimir.igo@gmail.com>
2018-08-26 22:00:27 +0300

    Updated KDE's autostart scripts

--- homedir/.config/startup.sh
+++ homedir/.config/startup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+plaympeg ~/Music/logonsound/* </dev/null &
+mount ~/mnt/yadisk 
+kstart --skiptaskbar kontact --iconify && sleep 10 && wmctrl -c Kontact
+#sleep 5 && wmctrl -c KTorrent
+sleep 5 && wmctrl -c Franz
+sleep 5 && conky -d
+sc-deskstop.py start
 
 #xinput --set-prop 9 'libinput Accel Profile Enabled' 1, 0
 #mplayer -really-quiet -vc null -vo null ~/Music/logonsound/* </dev/null &
-plaympeg ~/Music/logonsound/* </dev/null &
-mount ~/mnt/yadisk 
-#sleep 20 && kstart --skiptaskbar kontact --iconify
+#sleep 10 && kontact && sleep 2 && qdbus org.kde.kontact /kontact/MainWindow_1 org.qtproject.Qt.QWidget.hide
 #sleep 10 && kstart --skiptaskbar nixnote2
-sc-deskstop.py start
-ktorrent && wmctrl -c "ktorrent"
