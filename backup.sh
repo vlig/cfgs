96b2c052c0ee341a3de94b12ee84a680a8a55a87
Vladimir Smirnov <vladimir.igo@gmail.com>
2020-09-30 13:02:13 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 BKUPDIR="/mnt/SSD3/backup-arch"
-TAR="tar --xattrs -czpvf"
-EXT="tar.gz"
 
 backup ()
 {
-COMMAND="$TAR $OPTS $BKUPDIR/arch-$DEST.$EXT /$DEST"
+COMMAND="tar $OPTS --xattrs -czpvf $BKUPDIR/arch-$DEST.tar.gz /$DEST"
 echo -e "\n$COMMAND"
 echo -n "Backup /$DEST? (y/n): "; read b
 case "$b" in
@@ -24,17 +22,14 @@ echo "0: QUIT"
 echo -n "What to backup? "; read a
 case "$a" in
   1 ) DEST="efi"
-      OPTS=''
       backup;;
   2 ) DEST="/";
       OPTS='--exclude {"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/media/*","/lost+found/*","/swapfile","/home/*","/mnt/*"}'
       backup;;
   3 ) DEST="var"
-      OPTS=''
       backup;;
   4 ) DEST="home"
       OPTS='--exclude {"/home/*/.cache/*","/home/*/.ccache/*"}'
       backup;;
-  0 ) echo "Bye";;
-  * ) ;;
+  * ) echo "Bye";;
 esac
