668781002a6fd4160b99f1ceb7911d498335590a
vlig <vladimir.igo@gmail.com>
2021-06-08 20:12:26 +0300

    Updated rsync.sh

--- backup/rsync.sh
+++ backup/rsync.sh
@@ -2,25 +2,26 @@
 
 DEST=/ftp/backups
 OPTS='-aHAXPESh --info=progress2 --no-compress --numeric-ids --delete --delete-excluded --delete-after'
-EXCL='"/dev/","/proc/","/sys/","/tmp/","/run/","/media/","/lost+found/","/*/lost+found/","/swapfile","/var/swap","/mnt/","/var/empty/","/var/run/","/var/tmp/","/.snapshots/","/home/*/.gvfs/","/var/lib/dhcpcd/"'
-EXC2='"/home/*/.thumbnails/","/home/*/.cache/mozilla/","/home/*/.cache/chromium/","/home/*/.local/share/Trash/"'
-EXCB='"/usr/src/","/usr/ports/","/usr/doc/","/usr/obj/"'
+EXCL='"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/media/*","lost+found/","/swapfile","/var/swap","/mnt/*","/var/empty/*","/var/run/*","/var/tmp/*","/.snapshots/",".gvfs/","/var/lib/dhcpcd/",".ccache/",".cache/yay/**/src/"'
+EXC2='".thumbnails/",".cache/mozilla/",".cache/chromium/",".cache/google-chrome/",".local/share/Trash/"'
+EXCB='"/usr/src/*","/usr/ports/*","/usr/doc/*","/usr/obj/*"'
 SRC=/
 PORT=22
 CIPH='aes128-gcm@openssh.com'
 NOCONFIRM=false; RUN=REAL
+DATE='+%Y-%m-%d_%H-%M-%S'
 
-if (($(id -u) != 0)); then
-	echo "Must be root. Exiting..."; exit 2
-fi
-
-#echo; echo "Root login must be permitted."; echo
-#echo Syntax:
+#echo; echo "ssh root login must be permitted first!"; echo
+#echo Usage:
 #echo 'rsync.sh [-h|--host HOST] [-p|--port PORT] [--exclude-2] [--exclude-b] [-n|--dry-run] [-y|--no-confirm]'; echo
 #echo "--exclude-2 -- excludes from backup the following:"
 #echo "${EXC2}."
 #echo "--exclude-b -- excludes from backup the following (useful only for BSD hosts):"
 #echo "${EXCB}."
+
+if (($(id -u) != 0)); then
+	echo "You must be root. Exiting..."; exit 2
+fi
 
 TEMP=`getopt -o h:p:ny --long host:,port:,exclude-2,exclude-b,dry-run,no-confirm -n 'rsync.sh' -- "$@"`
 eval set -- "$TEMP"
@@ -36,6 +37,7 @@ while true; do
 	esac
 	shift
 done
+
 if [ -z "${HOST}" ]; then
 	HOST="$(hostname -s)"
 	HOST_LOG+=" (this host)"
@@ -70,8 +72,6 @@ if [ ${NOCONFIRM} = false ]; then
 	fi
 fi
 echo
-DATE='+%Y-%m-%d_%H-%M-%S'
-#DATE="$(date +%Y-%m-%d_%H-%M-%S)"
 TGT="${DIR}/last"
 #TGT="${DIR}/${DATE}"
 if [ ${RUN} = REAL ]; then
@@ -80,13 +80,10 @@ if [ ${RUN} = REAL ]; then
 		echo  "Target directory error" && exit 2
 	fi
 fi
+
 echo "$(date ${DATE}) -- ${RUN} backup start -- ${HOST_LOG}"
 eval "time ${CMD} ${TGT}"; echo
 [ $? -eq 0 ] && \
 { echo "$(date ${DATE}) -- ${RUN} backup done -- ${HOST_LOG}" && echo && exit 0; } || \
 { echo "$(date ${DATE}) -- ${RUN} backup done with errors! -- ${HOST_LOG}" && echo && exit 1; }
-
-#https://wiki.archlinux.org/index.php/rsync
-#https://www.pointsoftware.ch/2012/09/12/howto-local-and-remote-snapshot-backup-using-rsync-with-hard-links/
-#https://stackoverflow.com/a/7948533
 
