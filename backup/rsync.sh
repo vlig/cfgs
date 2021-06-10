b7b73c1218bec3154ed3394615fccd3bfbc8a519
vlig <vladimir.igo@gmail.com>
2021-06-10 18:35:46 +0300

    Updated rsync.sh

--- backup/rsync.sh
+++ backup/rsync.sh
@@ -1,89 +1,93 @@
 #!/usr/bin/env bash
 
-DEST=/ftp/backups
 OPTS='-aHAXPESh --info=progress2 --no-compress --numeric-ids --delete --delete-excluded --delete-after'
-EXCL='"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/media/*","lost+found/","/swapfile","/var/swap","/mnt/*","/var/empty/*","/var/run/*","/var/tmp/*","/.snapshots/",".gvfs/","/var/lib/dhcpcd/",".ccache/",".cache/yay/**/src/"'
-EXC2='".thumbnails/",".cache/mozilla/",".cache/chromium/",".cache/google-chrome/",".local/share/Trash/"'
+EXCL='"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/media/*","lost+found/","/swapfile","/var/swap","/mnt/*","/var/empty/*","/var/run/*","/var/tmp/*","/.snapshots/",".gvfs/","/var/lib/dhcpcd/"'
+EXC2='".thumbnails/",".cache/mozilla/",".cache/chromium/",".cache/google-chrome/",".local/share/Trash/",".ccache/",".cache/yay/**/src/"'
 EXCB='"/usr/src/*","/usr/ports/*","/usr/doc/*","/usr/obj/*"'
-SRC=/
 PORT=22
-CIPH='aes128-gcm@openssh.com'
-NOCONFIRM=false; RUN=REAL
-DATE='+%Y-%m-%d_%H-%M-%S'
-
-#echo; echo "ssh root login must be permitted first!"; echo
-#echo Usage:
-#echo 'rsync.sh [-h|--host HOST] [-p|--port PORT] [--exclude-2] [--exclude-b] [-n|--dry-run] [-y|--no-confirm]'; echo
-#echo "--exclude-2 -- excludes from backup the following:"
-#echo "${EXC2}."
-#echo "--exclude-b -- excludes from backup the following (useful only for BSD hosts):"
-#echo "${EXCB}."
 
 if (($(id -u) != 0)); then
-	echo "You must be root. Exiting..."; exit 2
+	echo "Must be executed as the superuser only. Exiting..."; exit 2
 fi
 
-TEMP=`getopt -o h:p:ny --long host:,port:,exclude-2,exclude-b,dry-run,no-confirm -n 'rsync.sh' -- "$@"`
+#echo "PERMIT SSH ROOT LOGIN TO SOURCE HOST FIRSTLY"
+#echo "usage: ./rsync.sh [--host HOST [--port PORT]] --dest DEST [OPTION] ..."
+#echo options:
+#echo "--exclude-2		exclude cashes, trash, etc. (check out the script)"
+#echo "--exclude-b		excludes for BSD hosts (check out the script)"
+#echo "--dry-run, -n		perform a trial run with no changes made"
+#echo "--no-confirm, -y		run non-interactively"
+#echo "--help, -h (*)           show this help (* -h is help only on its own)"
+
+TEMP=`getopt -o ny --long host:,port:,dest:,exclude-2,exclude-b,dry-run,no-confirm -n 'rsync.sh' -- "$@"`
 eval set -- "$TEMP"
 while true; do
 	case "$1" in
-		-h|--host)		HOST="$2"; shift ;;
-		-p|--port)		PORT="$2"; shift ;;
+		--host)			HOST="$2"; shift ;;
+		--port)			PORT="$2"; shift ;;
+		--dest)			DEST="$2"; shift ;;
 		--exclude-2)		EXCL+=",${EXC2}" ;;
 		--exclude-b)		EXCL+=",${EXCB}" ;;
-		-n|--dry-run)		OPTS+=" --dry-run"; RUN=TEST ;;
-		-y|--no-confirm)	NOCONFIRM=true ;;
+		-n|--dry-run)		OPTS+=" --dry-run"; RUN=" (in TEST mode)" ;;
+		-y|--no-confirm)	CONFIRM=false;;
 		?*)	break ;;
 	esac
 	shift
 done
+[ -z "${DEST}" ] && echo "No destination specified. Exiting..." && exit 1
 
 if [ -z "${HOST}" ]; then
+	SRC=/
 	HOST="$(hostname -s)"
-	HOST_LOG+=" (this host)"
+	HOST_LOG="${HOST} (this host)"
 	unset PORT
 else
-	HOST_LOG="${HOST}"
 	SRC="root@${HOST}:/"
+	HOST_LOG="${HOST}"
+	CIPH='aes128-gcm@openssh.com'
 	OPTS+=" -e \"ssh -T -c $CIPH -o Compression=no -x -i /root/.ssh/rsync -p $PORT\""
 fi
-DIR="${DEST}/${HOST}"
-if [ ${RUN} = REAL ]; then
+DIR="${DEST}${HOST}/"
+if [ -z ${RUN} ]; then
 	if [ ! -d ${DIR} ]; then
-		read -ern 1 -p "No ${DIR} directory. Create? [Y/n] "
+		read -ern 1 -p "${HOST} backup directory doesn\'t exist (${DIR}). Create? [Y/n] "
 		REPLY=${REPLY:-y}
 		if [[ "${REPLY}" = [Yy] ]]; then
 			mkdir ${DIR} && chmod 775 ${DIR} &&\
 			echo "Done. Ready for backup."
-			[ $? -ne 0 ] && { echo "Directory creation error." && exit 2; }
+			[ $? -ne 0 ] && { echo "${HOST} backup directory creation error." && exit 2; }
 		else
-			echo "No ${DIR} - no backup. Exiting..." && exit 1
+			echo "${HOST} backup directory creation rejected. Exiting..." && exit 1
 		fi
 	fi
 fi	
 
 CMD="rsync ${OPTS} --exclude={${EXCL}} ${SRC}"
-echo; echo "Command to do:"
-printf "${CMD} ${DIR}/\n"; echo
-if [ ${NOCONFIRM} = false ]; then
-	read -p "Type \"yes\" to start ${HOST_LOG} backup in ${RUN} mode: "
+echo "Command to do:"
+printf "${CMD} ${DIR}\n"
+if [[ -z ${CONFIRM} ]]; then
+	echo; read -p "Type \"yes\" to start full ${HOST_LOG} backup${RUN}: "
 	if [ "${REPLY}" != "yes" ]; then
 		echo "No \"yes\" typed. Exiting..."; exit 1
 	fi
 fi
-echo
-TGT="${DIR}/last"
-#TGT="${DIR}/${DATE}"
-if [ ${RUN} = REAL ]; then
+TGT="${DIR}last"
+#TGT="${DIR}$(LC_ALL=C date +%y%m%d-%a)"
+if [[ -z ${RUN} ]]; then
 	if [ ! -d ${TGT} ]; then
-		mkdir ${TGT} && chmod 775 ${TGT} ||\
+		mkdir ${TGT} && chmod 775 ${TGT} || \
 		echo  "Target directory error" && exit 2
 	fi
 fi
 
-echo "$(date ${DATE}) -- ${RUN} backup start -- ${HOST_LOG}"
-eval "time ${CMD} ${TGT}"; echo
-[ $? -eq 0 ] && \
-{ echo "$(date ${DATE}) -- ${RUN} backup done -- ${HOST_LOG}" && echo && exit 0; } || \
-{ echo "$(date ${DATE}) -- ${RUN} backup done with errors! -- ${HOST_LOG}" && echo && exit 1; }
+DATE="+%F %a %X"
+MSG="-- ${HOST_LOG} -- backup"
+echo
+echo "$(LC_ALL=C date "${DATE}") ${MSG} start${RUN} ..."
+eval time ${CMD} ${TGT}
+[ $? -eq 0 ] && { echo && \
+echo "$(LC_ALL=C date "${DATE}") ${MSG} completed successfully${RUN}" && \
+echo "Exiting..." && exit 0; } || { echo && \
+echo "$(LC_ALL=C date "${DATE}") ${MSG} completed with warning(s)${RUN}!" && \
+echo "You can run the script once more. Exiting..." && exit 1; }
 
