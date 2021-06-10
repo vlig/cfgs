73a306c878fb556e9068f3c68429449d446681b7
vlig <vladimir.igo@gmail.com>
2021-06-10 19:43:08 +0300

    Updated rsync-termux.sh

--- backup/rsync-termux.sh
+++ backup/rsync-termux.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-OPTS='-aHPESh --info=progress2 --no-compress --delete --delete-excluded --rsync-path="sudo rsync"'
+OPTS='-aHPESh --info=progress2 --no-compress --delete --delete-excluded --delete-after --rsync-path="sudo rsync"'
 SRC="~/storage/shared/"
 EXCL='"/Android/","/MIUI/backup/",'
 DEST="rsyncuser@192.168.66.40:/mnt/bu1/baksmb/ph1/int/"
@@ -9,13 +9,21 @@ DATE="$(date +%Y-%m-%d_%H:%M:%S)"
 HOST_LOG="$(hostname -s) (this host)"
 
 CMD="rsync ${OPTS} --exclude={${EXCL}} --rsh=\"${RSH}\" ${SRC} $DEST"
-echo; echo $CMD; echo
-read -p "Type \"yes\" to start ${HOST_LOG} backup, else exit: "
+echo "Command to do:"
+echo $CMD
+echo; read -p "Type \"yes\" to start ${HOST_LOG} backup: "
 if [ "${REPLY}" != "yes" ]; then
-	echo "No \"yes\" typed. Exiting..."; exit 0
+	echo "No \"yes\" typed. Exiting..."; exit 1
 fi
+DATE="+%F %a %X"
+MSG="-- ${HOST_LOG} -- backup"
 echo
-echo "${DATE} -- ${RUN} backup start -- ${HOST_LOG}"
-eval ${CMD} && echo &&\
-echo "${DATE} -- ${RUN} backup done -- ${HOST_LOG}" &&\
-exit 0
+echo "$(LC_ALL=C date "${DATE}") ${MSG} start ..."
+eval time ${CMD}
+#echo "${DATE} -- ${RUN} backup done -- ${HOST_LOG}" &&\
+[ $? -eq 0 ] && { echo && \
+echo "$(LC_ALL=C date "${DATE}") ${MSG} completed successfully" && \
+echo "Exiting..." && exit 0; } || { echo && \
+echo "$(LC_ALL=C date "${DATE}") ${MSG} completed with warning(s)" && \
+echo "You can run the script once more. Exiting..." && exit 1; }
+
